f30d9d664b7b802daf1f6d582e2d9e568b977838
RaulFR01 <rferrazrobles@hotmail.com>
2022-09-10 14:57:21 +0200

    iteration 1 done

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,88 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div id="app">
+    <h1>IronContacts</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="contact in ListOfContacts" :key="contact.name">
+          <td>
+            <img
+              :src="contact.pictureUrl"
+              :alt="`picture of` + contact.name"
+              style="width: 150px; height: 200px"
+            />
+          </td>
+          <td>
+            <p>{{ contact.name }}</p>
+          </td>
+          <td>
+            <p>{{ contact.popularity.toFixed(2) }}</p>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script>
+import contacts from "./contacts.json";
+export default {
+  data() {
+    return {
+      ListOfContacts: [
+        {
+          name: "Idris Elba",
+          pictureUrl:
+            "https://image.tmdb.org/t/p/w500/d9NkfCwczP0TjgrjpF94jF67SK8.jpg",
+          popularity: 11.622713,
+          id: "11731993-0604-4bee-80d5-67ad845d0a38",
+          wonOscar: false,
+          wonEmmy: false,
+        },
+        {
+          name: "Johnny Depp",
+          pictureUrl:
+            "https://image.tmdb.org/t/p/w500/kbWValANhZI8rbWZXximXuMN4UN.jpg",
+          popularity: 15.656534,
+          id: "7dad00f7-3949-477d-a7e2-1467fd2cfc06",
+          wonOscar: false,
+          wonEmmy: false,
+        },
+        {
+          name: "Monica Bellucci",
+          pictureUrl:
+            "https://image.tmdb.org/t/p/w500/qlT4904d8oi2NIs28RrgnIZDFZB.jpg",
+          popularity: 16.096436,
+          id: "0ad5e441-3084-47a1-9e9b-b917539bba71",
+          wonOscar: false,
+          wonEmmy: false,
+        },
+        {
+          name: "Gal Gadot",
+          pictureUrl:
+            "https://image.tmdb.org/t/p/w500/34kHAyBaBhq2kUrmhM15paEBuuI.jpg",
+          popularity: 10.049256,
+          id: "b497e3c4-50bb-4ae2-912f-eb36802c5bc2",
+          wonOscar: false,
+          wonEmmy: false,
+        },
+        {
+          name: "Ian McKellen",
+          pictureUrl:
+            "https://image.tmdb.org/t/p/w500/coWjgMEYJjk2OrNddlXCBm8EIr3.jpg",
+          popularity: 10.070132,
+          id: "0067ae32-97b6-4431-898e-eb1c10150abb",
+          wonOscar: false,
+          wonEmmy: false,
+        },
+      ],
+    };
+  },
+};
+</script>
